b05a73cd5bb7e69f5017d31882c766d8fefccdd8
setZ <ramiras.ukr@gmail.com>
2018-10-18 16:14:35 +0300

    1

--- task_2/common.js
+++ task_2/common.js
@@ -1,7 +1,7 @@
-function CheckTheNumberOfNumbers(){
-	var InputForEnterData = document.getElementById('InputForEnterData').value;
+function checkTheNumberOfNumbers(){
+	var inputForEnterData = document.getElementById('inputForEnterData').value;
 	var result = document.getElementById('result');
-	var res = InputForEnterData.match(/\d/g).length;
-	
+	var res = inputForEnterData.match(/\d/g).length;
+
 	res%2 == 0 ? result.innerHTML = 'В строке парное количество цифр' : result.innerHTML = 'В строке НЕ парное количество цифр';	
 }
