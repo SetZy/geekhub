2b42714a7b9e4c9179296e98fc0330b864b86d69
setZ <ramiras.ukr@gmail.com>
2018-10-18 16:21:53 +0300

    Enter space between '%'

--- task_2/common.js
+++ task_2/common.js
@@ -3,5 +3,5 @@ function checkTheNumberOfNumbers(){
 	var result = document.getElementById('result');
 	var res = inputForEnterData.match(/\d/g).length;
 
-	res%2 == 0 ? result.innerHTML = 'В строке парное количество цифр' : result.innerHTML = 'В строке НЕ парное количество цифр';	
+	res % 2 == 0 ? result.innerHTML = 'В строке парное количество цифр' : result.innerHTML = 'В строке НЕ парное количество цифр';	
 }
