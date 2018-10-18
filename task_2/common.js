17a8a2b594b020a2b15cb17257da6a6835a221ff
setZ <ramiras.ukr@gmail.com>
2018-10-18 12:07:08 +0300

    Enter task2

---
+++ task_2/common.js
@@ -0,0 +1,25 @@
+function func(){
+	var input1 = document.getElementById('input1').value;
+	var arr = input1.split('');
+	var n = 0;
+	var result = document.getElementById('result')
+
+	for(var i = 0; i < arr.length; i++){
+		var check = parseInt(arr[i])
+		if (isNaN(check)) {
+			n = n;
+		} else {
+			n++
+		}
+
+		if (n/2==0) {
+			result.innerHTML = 'В строке парное количество цифр'
+		} else {
+			result.innerHTML = 'В строке НЕ парное количество цифр'
+		}
+	}
+
+	
+	
+	
+}
