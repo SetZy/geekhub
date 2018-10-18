c1be83fc5dc161a3bc783c1903489c66b17cf472
setZ <ramiras.ukr@gmail.com>
2018-10-18 14:06:00 +0300

    Update task_2/common.js

--- task_2/common.js
+++ task_2/common.js
@@ -1,9 +1,17 @@
 function func(){
 	var input1 = document.getElementById('input1').value;
-	var arr = input1.split('');
+	var result = document.getElementById('result');
+	var res = input1.split(/\d/).length-1;
+	if (res%2 == 0) {
+		result.innerHTML = 'В строке парное количество цифр'
+	} else {
+		result.innerHTML = 'В строке НЕ парное количество цифр'
+	}
+	alert(res)
+	
+	
+	/*var arr = input1.split('');
 	var n = 0;
-	var result = document.getElementById('result')
-
 	for(var i = 0; i < arr.length; i++){
 		var check = parseInt(arr[i])
 		if (isNaN(check)) {
@@ -18,7 +26,8 @@ function func(){
 		} else {
 			result.innerHTML = 'В строке НЕ парное количество цифр'
 		}
-	}
+	}*/
+	
 
 	
 	
